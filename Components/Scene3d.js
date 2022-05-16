87971a73693090c82d8d663226a2116021e14222
Sergey Kozlov <zeemoney@yandex.ru>
2022-05-16 19:43:07 +0300

    1

--- Components/Scene3d.js
+++ Components/Scene3d.js
@@ -1,39 +1,25 @@
 import { Suspense, useEffect, useState } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Environment } from '@react-three/drei'
-import { animated, useSpring } from '@react-spring/web'
 
 import User3d from './User3d.js'
 import Phone3d from './Phone3d.js'
 import Model404 from './Model404.js'
 
-const FadeIn = ({ children }) => {
-	const styles = useSpring({
-		from: {
-			opacity: 0,
-			height: '100%',
-			widows: '100%',
-			transition: 'all 1.0s ease 0s',
-		},
-		to: {
-			opacity: 1,
-			height: '100%',
-			widows: '100%',
-			transition: 'all 1.0s ease 0s',
-		},
-	})
-
-	return <animated.div style={styles}>{children}</animated.div>
-}
-
 export const Scene3d = props => {
-	const [rotationSpeed, setRotationSpeed] = useState(100)
+	const [rotationSpeed, setRotationSpeed] = useState(1)
+
+	const fastRotate = (speed = 500) => {
+		;(async () => {
+			await setRotationSpeed(speed)
+			await setTimeout(() => {
+				setRotationSpeed(1)
+			}, 1000)
+		})()
+	}
 
 	useEffect(() => {
-		const lowSpeed = () => {
-			setRotationSpeed(1)
-		}
-		setTimeout(lowSpeed, 1000)
+		fastRotate()
 	}, [])
 
 	let model = <Phone3d />
@@ -47,7 +33,6 @@ export const Scene3d = props => {
 			model = (
 				<>
 					<User3d />
-					{/* <Window /> */}
 				</>
 			)
 			break
@@ -60,7 +45,6 @@ export const Scene3d = props => {
 			model = (
 				<>
 					<User3d />
-					{/* <Window /> */}
 				</>
 			)
 			break
@@ -68,24 +52,27 @@ export const Scene3d = props => {
 
 	return (
 		<div className='scene3d__wrapper'>
-			<FadeIn>
-				<Canvas shadowMap camera={{ position: [-7, 2.8, 3], zoom: 2, fov: 58 }}>
-					<mesh>
-						<OrbitControls
-							autoRotate={true}
-							enableZoom={false}
-							enableRotate={true}
-							enablePan={false}
-							autoRotateSpeed={rotationSpeed}
-						/>
-						<ambientLight intensity={1} />
-						<Environment preset='warehouse' />
-						<directionalLight position={[10, 10, 5]} intensity={1} />
-						<pointLight position={[2, 3, 5]} intensity={0.03} />
+			<Canvas shadowMap camera={{ position: [-7, 2.8, 3], zoom: 2, fov: 58 }}>
+				<mesh
+					onClick={() => {
+						fastRotate(1000)
+					}}
+				>
+					<OrbitControls
+						autoRotate={true}
+						enableZoom={false}
+						enableRotate={false}
+						enablePan={false}
+						autoRotateSpeed={rotationSpeed}
+					/>
+					<ambientLight intensity={1} />
+					<Environment preset='warehouse' />
+					<directionalLight position={[10, 10, 5]} intensity={1} />
+					<pointLight position={[2, 3, 5]} intensity={0.03} />
 
-						<Suspense fallback={null}>{model}</Suspense>
-					</mesh>
-					{/* <mesh
+					<Suspense fallback={null}>{model}</Suspense>
+				</mesh>
+				{/* <mesh
 						position={[0, -1, 0]}
 						rotation={[-Math.PI / 2, 0, 0]}
 						receiveShadow
@@ -93,8 +80,7 @@ export const Scene3d = props => {
 						<planeBufferGeometry attach='geometry' args={[1000, 1000]} />
 						<meshPhongMaterial attach='material' color='#2c2c2c' />
 					</mesh> */}
-				</Canvas>
-			</FadeIn>
+			</Canvas>
 		</div>
 	)
 }
