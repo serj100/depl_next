c486770b1212abebb2eb23f21a5a4dc5bdf74c3c
Sergey Kozlov <zeemoney@yandex.ru>
2022-05-16 20:36:28 +0300

    1

--- Components/ExampleLayouts.js
+++ Components/ExampleLayouts.js
@@ -1,4 +1,5 @@
 import { useSelector } from 'react-redux'
+import Image from 'next/image'
 
 export const ExampleLayouts = () => {
 	const language = useSelector(state => state.language.status)
@@ -14,9 +15,17 @@ export const ExampleLayouts = () => {
 			</div>
 			<div className='layouts__wrapper'>
 				<div className='examples__wrapper _anim-items _start'>
-					<div className='example'></div>
-					<div className='example'></div>
-					<div className='example'></div>
+					<div className='example'>
+						<div className='img'>
+							<a target='_blank' href='http://f0673816.xsph.ru/project1/'>
+								<Image
+									width={260}
+									height={130}
+									src='/img/project1/project1.png'
+								/>
+							</a>
+						</div>
+					</div>
 				</div>
 			</div>
 		</>
