dfc4cc2fcb2b4e40ea21493da1efaaccfac3bffb
Sergey Kozlov <zeemoney@yandex.ru>
2022-05-18 13:29:36 +0300

    1

--- Components/Layout.js
+++ Components/Layout.js
@@ -3,37 +3,13 @@ import { Footer } from '../Components/Footer'
 
 const Layout = ({ children }) => {
 	return (
-		<>
-			<div className='container'>
-				<Header />
-				<div className='container__content'>
-					{children}
-					<Footer />
-				</div>
+		<div className='container'>
+			<Header />
+			<div className='container__content'>
+				{children}
+				<Footer />
 			</div>
-			<div class='background'>
-				<span></span>
-				<span></span>
-				<span></span>
-				<span></span>
-				<span></span>
-				<span></span>
-				<span></span>
-				<span></span>
-				<span></span>
-				<span></span>
-				<span></span>
-				<span></span>
-				<span></span>
-				<span></span>
-				<span></span>
-				<span></span>
-				<span></span>
-				<span></span>
-				<span></span>
-				<span></span>
-			</div>
-		</>
+		</div>
 	)
 }
 
