ea176fa93ead3b98097a29c77e671f25f7ef77af
Sergey Kozlov <zeemoney@yandex.ru>
2022-05-22 04:24:28 +0300

    1

--- Components/AboutMe.js
+++ Components/AboutMe.js
@@ -9,16 +9,21 @@ export const AboutMe = () => {
 			<div className='img'>
 				<Image width={960} height={1280} src='/img/photo.jpg' alt='' />
 			</div>
-			<h3>{language === 'ru' ? 'Сергей Козлов' : 'Sergey Kozlov'}</h3>
-			<div className='secont_subtitle__wrapper'>
-				<p> {language === 'ru' ? 'Немного обо мне:' : 'Something about me:'}</p>
-			</div>
-			<div className='last_subtitle__wrapper'>
-				<p>
-					{language === 'ru'
-						? `Занимаюсь версткой шаблонов сайтов. Пишу на JavaScript. Любимые технологии - NodeJS, React JS, React Native, Gulp❤️, webpack, SCSS.`
-						: `I live and work in Russia. Freelancing in the field web templates sites. Favoritetechnologies - NodeJS, React JS, React Native, Gulp ❤️, webpack, SCSS.`}
-				</p>
+			<div className='decsription'>
+				<h3>{language === 'ru' ? 'Сергей Козлов' : 'Sergey Kozlov'}</h3>
+				<div className='secont_subtitle__wrapper'>
+					<p>
+						{' '}
+						{language === 'ru' ? 'Немного обо мне:' : 'Something about me:'}
+					</p>
+				</div>
+				<div className='last_subtitle__wrapper'>
+					<p>
+						{language === 'ru'
+							? `Занимаюсь версткой шаблонов сайтов. Пишу на JavaScript. Любимые технологии - NodeJS, React JS, React Native, Gulp❤️, webpack, SCSS.`
+							: `I live and work in Russia. Freelancing in the field web templates sites. Favoritetechnologies - NodeJS, React JS, React Native, Gulp ❤️, webpack, SCSS.`}
+					</p>
+				</div>
 			</div>
 		</div>
 	)
