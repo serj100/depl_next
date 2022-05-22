eeb7c2e9762e04ee66ed95fe9885350665b8c113
Sergey Kozlov <zeemoney@yandex.ru>
2022-05-22 04:03:46 +0300

    1

--- Components/AboutMe.js
+++ Components/AboutMe.js
@@ -1,13 +1,17 @@
 import { useSelector } from 'react-redux'
+import Image from 'next/image'
 
 export const AboutMe = () => {
 	const language = useSelector(state => state.language.status)
 
 	return (
 		<div className='about_me__wrapper'>
+			<div className='img'>
+				<Image width={960} height={960} src='/img/photo.jpg' alt='' />
+			</div>
 			<h3>{language === 'ru' ? 'Сергей Козлов' : 'Sergey Kozlov'}</h3>
 			<div className='secont_subtitle__wrapper'>
-				<p> {language === 'ru' ? 'Немного обо мне' : 'Something about me:'}</p>
+				<p> {language === 'ru' ? 'Немного обо мне:' : 'Something about me:'}</p>
 			</div>
 			<div className='last_subtitle__wrapper'>
 				<p>
