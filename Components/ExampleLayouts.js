778cbfe766f0fe774b2a0b23d9f353781bd224e3
Sergey Kozlov <zeemoney@yandex.ru>
2022-05-22 03:26:45 +0300

    1

--- Components/ExampleLayouts.js
+++ Components/ExampleLayouts.js
@@ -10,7 +10,7 @@ export const ExampleLayouts = () => {
 				<p>
 					{language == 'en'
 						? `Examples of the quality of my layout:`
-						: `Примеры качества верстки макетов:`}
+						: `Примеры верстки макетов:`}
 				</p>
 			</div>
 			<div className='layouts__wrapper'>
@@ -26,6 +26,22 @@ export const ExampleLayouts = () => {
 									width={290}
 									height={160}
 									src='/img/project1/project1.png'
+									alt=''
+								/>
+							</a>
+						</div>
+					</div>
+					<div className='example'>
+						<div className='img'>
+							<a
+								target='_blank'
+								rel='noopener noreferrer'
+								href='http://f0673816.xsph.ru/project2/'
+							>
+								<Image
+									width={290}
+									height={160}
+									src='/img/project1/project2.png'
 									alt=''
 								/>
 							</a>
